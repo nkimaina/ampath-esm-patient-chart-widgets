ebf0ded235b8a9b2c143f6eafbeaa6ea6540d460
Nicky Kimaina <nkimaina@ampath.or.ke>
2020-03-30 14:24:01 +0300

    Added ability to configure forms

--- src/visit/visit-summary.component.tsx
+++ src/visit/visit-summary.component.tsx
@@ -1,10 +1,20 @@
 import React, { useState } from "react";
+import {
+  defineConfigSchema,
+  ModuleNameContext
+} from "@openmrs/esm-module-config";
 
 import StartVisitComponent from "./start-visit.component";
 import useStartedVisit from "./use-started-visit";
 import styles from "../summary-card.css";
+import { esmPatientChartWidgetsSchema } from "../config/patient-chart-widgets.schema";
 
 export default function VisitSummaryComponent(props: any) {
+  defineConfigSchema(
+    "@ampath/esm-patient-chart-widgets",
+    esmPatientChartWidgetsSchema
+  );
+
   const visitToStart: VisitTypeProp = {
     // Intentional: useful for switching between openmrs-spa and ampath environment for testing
     // visitTypeUuid: "7b0f5697-27e3-40c4-8bae-f4049abfb4ed",
@@ -31,82 +41,86 @@ export default function VisitSummaryComponent(props: any) {
   };
 
   return (
-    <div
-      style={{
-        minWidth: "20rem",
-        textAlign: "left"
-      }}
-      className={`omrs-card ${styles.card}`}
-    >
-      <div className={styles.header}>
-        <div className={`${styles.headerTitle}`}>
-          <h2 className="omrs-padding-left-16">
-            {startedVisit
-              ? "Today's Outpatient Visit"
-              : "Start New Clinical Visit"}
-          </h2>
-        </div>
-      </div>
+    <ModuleNameContext.Provider value="@ampath/esm-patient-chart-widgets">
       <div
-        style={{ maxHeight: "320px", minHeight: "320px", overflow: "scroll" }}
+        style={{
+          minWidth: "20rem",
+          textAlign: "left"
+        }}
+        className={`omrs-card ${styles.card}`}
       >
-        {!startingVisit && startedVisit === undefined && <div>Loading...</div>}
-        {startingVisit && (
-          <StartVisitComponent
-            visitType={visitToStart}
-            onVisitStarted={onVisitStarted}
-            onCanceled={onVisitStartingCancelled}
-          />
-        )}
-        {!startingVisit && startedVisit === null && (
-          <div
-            className="omrs-padding-top-4 omrs-padding-bottom-4"
-            style={{
-              borderBottom: "1px solid var(--omrs-color-bg-low-contrast)",
-              display: "flex",
-              justifyContent: "flex-start",
-              flexDirection: "row"
-            }}
-          >
-            <div style={{}}>
-              <button
-                className="omrs-btn omrs-text-action"
-                onClick={() => startVisit(null)}
-              >
-                Start {visitToStart.visitDisplay}
-              </button>
-            </div>
+        <div className={styles.header}>
+          <div className={`${styles.headerTitle}`}>
+            <h2 className="omrs-padding-left-16">
+              {startedVisit
+                ? "Today's Outpatient Visit"
+                : "Start New Clinical Visit"}
+            </h2>
+          </div>
+        </div>
+        <div
+          style={{ maxHeight: "320px", minHeight: "320px", overflow: "scroll" }}
+        >
+          {!startingVisit && startedVisit === undefined && (
+            <div>Loading...</div>
+          )}
+          {startingVisit && (
+            <StartVisitComponent
+              visitType={visitToStart}
+              onVisitStarted={onVisitStarted}
+              onCanceled={onVisitStartingCancelled}
+            />
+          )}
+          {!startingVisit && startedVisit === null && (
             <div
-              className="omrs-margin-right-4 omrs-padding-top-4"
-              style={{ marginLeft: "auto" }}
+              className="omrs-padding-top-4 omrs-padding-bottom-4"
+              style={{
+                borderBottom: "1px solid var(--omrs-color-bg-low-contrast)",
+                display: "flex",
+                justifyContent: "flex-start",
+                flexDirection: "row"
+              }}
+            >
+              <div style={{}}>
+                <button
+                  className="omrs-btn omrs-text-action"
+                  onClick={() => startVisit(null)}
+                >
+                  Start {visitToStart.visitDisplay}
+                </button>
+              </div>
+              <div
+                className="omrs-margin-right-4 omrs-padding-top-4"
+                style={{ marginLeft: "auto" }}
+              >
+                <svg
+                  className="omrs-icon"
+                  fill="var(--omrs-color-ink-low-contrast)"
+                >
+                  <use xlinkHref="#omrs-icon-chevron-right"></use>
+                </svg>
+              </div>
+            </div>
+          )}
+
+          {startedVisit && (
+            <div
+              className="omrs-type-body-regular omrs-padding-top-4 omrs-margin-left-16"
+              style={{ color: "var(--omrs-color-interaction)" }}
             >
               <svg
                 className="omrs-icon"
-                fill="var(--omrs-color-ink-low-contrast)"
+                fill="var(--omrs-color-interaction)"
+                style={{ height: "1rem", width: "1rem" }}
               >
-                <use xlinkHref="#omrs-icon-chevron-right"></use>
-              </svg>
+                <use xlinkHref="#omrs-icon-check-circle"></use>
+              </svg>{" "}
+              Started Outpatient Visit
             </div>
-          </div>
-        )}
-
-        {startedVisit && (
-          <div
-            className="omrs-type-body-regular omrs-padding-top-4 omrs-margin-left-16"
-            style={{ color: "var(--omrs-color-interaction)" }}
-          >
-            <svg
-              className="omrs-icon"
-              fill="var(--omrs-color-interaction)"
-              style={{ height: "1rem", width: "1rem" }}
-            >
-              <use xlinkHref="#omrs-icon-check-circle"></use>
-            </svg>{" "}
-            Started Outpatient Visit
-          </div>
-        )}
+          )}
+        </div>
       </div>
-    </div>
+    </ModuleNameContext.Provider>
   );
 }
 
