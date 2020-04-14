1369cb0689e30eed43f6f8e6d9dcdc469a21ecf4
Nicky Kimaina <nkimaina@ampath.or.ke>
2020-04-14 16:43:52 +0300

    Added ability to fetch encounters by date

--- src/openmrs-resource/encounter.resource.ts
+++ src/openmrs-resource/encounter.resource.ts
@@ -2,17 +2,26 @@ import { openmrsObservableFetch } from "@openmrs/esm-api";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import { Form, toFormObject } from "./form.resource";
+import { toOmrsDateString } from "../utils/omrs-dates";
 
 export function getPatientEncounters(
-  patientUuid: string
+  patientUuid: string,
+  v = "full",
+  startDate?: Date,
+  endDate?: Date
 ): Observable<Array<Encounter>> {
-  return openmrsObservableFetch(
-    `/ws/rest/v1/encounter?v=full&patient=${patientUuid}`
-  ).pipe(
+  let url = `/ws/rest/v1/encounter?v=${v}&patient=${patientUuid}`;
+  if (startDate) {
+    url = `${url}&fromdate=${startDate.toISOString()}`;
+  }
+  if (endDate) {
+    url = `${url}&todate=${endDate.toISOString()}`;
+  }
+  return openmrsObservableFetch(url).pipe(
     map(results => {
-      const encounters: Encounter[] = results["data"]["results"].map(
-        encounter => toEncounterObject(encounter)
-      );
+      const encounters: Encounter[] = results["data"][
+        "results"
+      ].map(encounter => toEncounterObject(encounter));
       return encounters;
     })
   );
